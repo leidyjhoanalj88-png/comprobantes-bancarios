36f83122844cb71bfedc14264ec48395b2f7854b
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-03-11 00:59:40 -0500

    Actualizar app.py

--- app.py
+++ app.py
@@ -127,7 +127,8 @@ def toggle_usuario(uid):
     conn.close()
     return jsonify({'ok': True})
 
-if __name__ == '__main__':
-    init_db()
+init_db()
+
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
